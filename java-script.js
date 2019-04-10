be2617e0cd3996ee8b35f717f6b735ac6e3890c7
Bryan B <ohiomagic34@gmail.com>
2019-04-09 22:17:13 -0600

    Initial commit

---
+++ java-script.js
@@ -0,0 +1,8 @@
+const main = document.querySelector('main');
+for (i = 0; i < 16; i++) {
+    const div = document.createElement('div');
+    div.classList.add('java-div')
+    main.appendChild(div);
+}
+
+
