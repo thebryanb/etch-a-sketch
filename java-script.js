cfad36fd9577f117fc6dc8115051de70def1d199
Bryan B <ohiomagic34@gmail.com>
2019-04-10 23:05:08 -0600

    Add js to change div color

--- java-script.js
+++ java-script.js
@@ -5,4 +5,15 @@ for (i = 0; i < 16; i++) {
     main.appendChild(div);
 }
 
+function changeColor(e) {
+    console.log(e.target);
+    e.target.style.cssText = "background: blue;";
+}
+
+
+let divArray = document.querySelectorAll('div');
+divArray.forEach(function(element) {
+    element.addEventListener('mouseover',changeColor);
+});
+
 
