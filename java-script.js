af869e74a8ee377917e22bc49f26825034f06000
Bryan B <ohiomagic34@gmail.com>
2019-04-11 01:05:12 -0600

    Add button functionality
    Create CSS variable to control grid size
    Place div creation js code in function
    Create js function to update CSS grid variables

--- java-script.js
+++ java-script.js
@@ -1,19 +1,53 @@
+function updateGrid(size) {
+    main.style.setProperty('--numCols', size);
+    main.style.setProperty('--numRows', size);
+}
+function populateGrid(size) {
+    const numDivs = size*size;
+    for (i = 0; i < numDivs; i++) {
+        const div = document.createElement('div');
+        div.classList.add('java-div')
+        main.appendChild(div);
+    }
+    divArray = document.querySelectorAll('div.java-div');
+    window.addDivListeners();
+}
+function addDivListeners() {
+    divArray.forEach(function(element) {
+        element.addEventListener('mouseover',changeColor);
+    });
+}
+function changeColor(e, color='blue') {
+    // console.log(e.target);
+    e.target.style.background = color;
+}
+function clearGrid() {
+    console.log('clear grid')
+    divArray.forEach(function(element) { 
+        element.style.background = 'none';
+    });
+}
+function resizeGrid() {
+    const newSize = Number(prompt('How many squares per side?'));
+    divArray.forEach(function(element) {
+        main.removeChild(element);
+    });
+    console.log(newSize);
+    window.updateGrid(newSize);
+    window.populateGrid(newSize);
+}
+
+let btnClear = document.getElementById('clear-btn');
+let btnResize = document.getElementById('resize-btn');
+btnClear.addEventListener('click', clearGrid);
+btnResize.addEventListener('click', resizeGrid);
+
 const main = document.querySelector('main');
-for (i = 0; i < 16; i++) {
-    const div = document.createElement('div');
-    div.classList.add('java-div')
-    main.appendChild(div);
-}
-
-function changeColor(e) {
-    console.log(e.target);
-    e.target.style.cssText = "background: blue;";
-}
+let divArray;
+window.populateGrid(4);
 
 
-let divArray = document.querySelectorAll('div.java-div');
-divArray.forEach(function(element) {
-    element.addEventListener('mouseover',changeColor);
-});
+
+
 
 
